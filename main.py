44aa011717382aff4c6f71fc969a37154916ce0b
Filip Pascuti <filip@192-168-0-100.rdsnet.ro>
2022-01-06 19:25:57 +0200

    finished lab4

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from typing import Tuple
+from typing import Tuple, List
+from collections import defaultdict
+import math
+import random
+import sympy
 
-alphabet = " abcdefghijklmnopqrstuvwxyz"
+
+# alphabet = " abcdefghijklmnopqrstuvwxyz"
 
 
 def print_problem_statement():
@@ -17,78 +22,7 @@ def print_problem_statement():
 
 
 def generate_keys(start: int, end: int) -> Tuple:
-    pass
-
-
-def encrypt(plaintext: str, public_key) -> str:
-    pass
-
-
-def decrypt(ciphertext: str, private_key) -> str:
-    pass
-
-
-def main():
-    print_problem_statement()
-
-    start = int(input(">>>lower bound: \n"))
-    end = int(input(">>>upper bound: \n"))
-
-    public_key, private_key = generate_keys(start, end)
-    print(f"The public key is: {public_key} and the private key is {private_key}")
-
-    plaintext = input("text to encrypt: \n")
-    encrypted_text = encrypt(plaintext, public_key)
-    print(f"The encrypted text is: {encrypted_text}")
-
-    ciphertext = input("text to decrypt: \n")
-    decrypted_text = decrypt(ciphertext, private_key)
-    print(f"The decrypted text is: {decrypted_text}")
-
-
-if __name__ == "__main__":
-    main()
-
-import math
-import random
-
-from cffi.backend_ctypes import xrange
-
-
-def miller_rabin(n, k=10000):
-    if n == 2:
-        return True
-
-    if n % 2 == 0:
-        return False
-
-    r, s = 0, n - 1
-    while s % 2 == 0:
-        r += 1
-        s //= 2
-    for _ in xrange(k):
-        a = random.randrange(2, n - 1)
-        x = pow(a, s, n)
-        if x == 1 or x == n - 1:
-            continue
-        for _ in xrange(r - 1):
-            x = pow(x, 2, n)
-            if x == n - 1:
-                break
-        else:
-            return False
-    return True
-
-
-def generate_primes():
-    x = 10
-    while not miller_rabin(x):
-        x = random.randrange(pow(10, 20), pow(10, 40))
-    return x
-
-
-def generate_public_key():
-    p = generate_primes()
+    p = sympy.randprime(start, end)
     g = random.randint(2, p - 1)
     a = random.randint(1, p - 2)
     public_key = [p, g, pow(g, a, p)]
@@ -97,104 +31,79 @@ def generate_public_key():
     return public_key, private_key
 
 
-def compute_alphabet():
-    alphabet = {' ': 10}
+def get_symmetric_letters_to_numbers():
+    alphabet = {" ": 10, 10: " "}
     val = ord('a')
-    for idx in range(1, 27):
-        alphabet[chr(val + idx - 1)] = idx + 10
-    val = ord('A')
-    count = 27
-    for idx in range(1, 27):
-        alphabet[chr(val + idx - 1)] = count + 10
-        count += 1
+    for idx in range(0, 26):
+        letter, key = chr(val + idx), idx + 11
+        alphabet[letter] = key
+        alphabet[key] = letter
     return alphabet
-
-
-def compute_inv_alphabet(alphabet):
-    inv_alphabet = {}
-    for char, num in alphabet.items():
-        inv_alphabet[num] = char
-    return inv_alphabet
 
 
 def translate_plaintext_into_numerical(plaintext, alphabet):
     numerical = []
     for char in plaintext:
-        numerical.append(alphabet[char])
-    return numerical
+        numerical.append(str(alphabet[char]))
+    return int("".join(numerical))
 
 
-def translate_numerical_into_plaintext(numerical, inv_alphabet):
+def translate_numerical_into_plaintext(numerical, alphabet):
     plaintext = ''
-    for num in numerical:
-        plaintext += inv_alphabet[num]
+    numerical = str(numerical)
+    for index in range(0, len(numerical), 2):
+        num = int(numerical[index: index+2])
+        plaintext += alphabet[num]
     return plaintext
 
 
-def encrypt(public_key, private_key, m):
-    p = public_key[0]
-    g = public_key[1]
+def encrypt(plaintext: str, public_key) -> List:
+    message = translate_plaintext_into_numerical(plaintext, alphabet=get_symmetric_letters_to_numbers())
+
+    p, g, g_a = public_key
+
     k = random.randint(1, p - 2)
+
     alpha = pow(g, k, p)
-    beta = (m * pow(pow(g, private_key, p), k, p)) % p
-    return alpha, beta
+
+    beta = (message * pow(g_a, k, p)) % p
+
+    return [alpha, beta]
 
 
-def decrypt(public_key, private_key, alpha, beta):
-    p = public_key[0]
-    return (pow(alpha, p - 1 - private_key, p) * beta) % p
+def decrypt(ciphertext: List, public_key, private_key) -> str:
+    alpha, beta = ciphertext
+    p, g, g_a = public_key
+
+    message = (pow(alpha, -private_key, p) * beta) % p
+
+    message = translate_numerical_into_plaintext(message, alphabet=get_symmetric_letters_to_numbers())
+
+    return message
 
 
-def convert_message_into_number(message, public_key):
-    p = public_key[0]
-    if message <= p - 1:
-        return [message]
-    else:
-        slice = math.ceil(len(str(message)) / len(str(p - 1)))
-        xxx = len(str(p - 1))
-        m = str(message)
-        parts = []
-        for i in range(0, slice):
-            if i == slice - 1:
-                part = m[i * xxx:len(m) + 1]
-            else:
-                part = m[i * xxx:(i + 1) * xxx]
-            parts.append(part)
-        return parts
+def main():
+    # print_problem_statement()
+
+    # plaintext = input("text to encrypt: \n")
+    # plaintext = "a b c d e f g h i j k l m n o p q r s t u v x y z a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a  a a a a a a a a a a a a a a  a aa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasasasasasasasasasasasasasasas"
+    plaintext = "a" * 500
+    # start = int(input(">>>lower bound: \n"))
+    start = 10 ** (len(plaintext) * 2)
+    # end = int(input(">>>upper bound: \n"))
+    end = 10 ** (len(plaintext) * 3)
+
+    public_key, private_key = generate_keys(start, end)
+    print(f"The public key is: {public_key} and the private key is {private_key}")
+
+    encrypted_text = encrypt(plaintext, public_key)
+    print(f"The encrypted text is: {encrypted_text}")
+
+    # ciphertext = input("text to decrypt: \n")
+    ciphertext = encrypted_text
+    decrypted_text = decrypt(ciphertext, public_key, private_key)
+    print(f"The decrypted text is: {decrypted_text}")
 
 
-if __name__ == '__main__':
-    public_key, private_key = generate_public_key()
-
-    input = "abcdef  ghqaslklksdhfoia faaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ASKFLAIW ASLFIAISFA afajaw ASFASFdgsegs"
-    alphabet = compute_alphabet()
-    msg = translate_plaintext_into_numerical(input, alphabet)
-    print("Input = " + input)
-    m = ''
-    for el in msg:
-        m += str(el)
-    m = int(m)
-
-    p = public_key[0]
-
-    m_num = convert_message_into_number(m, public_key)
-
-    list_alpha = []
-    list_beta = []
-    for el in m_num:
-        alpha, beta = encrypt(public_key, private_key, int(el))
-        list_alpha.append(alpha)
-        list_beta.append(beta)
-
-    decrypted_text = ""
-    decrypted_number = ""
-    for i in range(len(list_alpha)):
-        output = decrypt(public_key, private_key, list_alpha[i], list_beta[i])
-        decrypted_number += str(output)
-
-    list_numbers = []
-    for i in range(0, len(decrypted_number), 2):
-        number = decrypted_number[i] + decrypted_number[i + 1]
-        list_numbers.append(int(number))
-    decrypted_text = translate_numerical_into_plaintext(list_numbers, compute_inv_alphabet(alphabet))
-    print("Output = " + decrypted_text)
+if __name__ == "__main__":
+    main()
